31facd7829055ec261e469338aa997f2a3a98457
Mayank Chouhan <mynkchnn@gmail.com>
2026-06-01 22:06:54 +0530

    Load database URL from environment variables

--- backend/database_models.py
+++ backend/database_models.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine, Column, String, Integer, DateTime, Text, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 Base = declarative_base()
-engine = create_engine("sqlite:///./app.db", connect_args={"check_same_thread": False})
+engine = create_engine(os.getenv('DATABASE_URL'), connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine,autocommit=False,autoflush=False,expire_on_commit=False)
 
 class User(Base):
